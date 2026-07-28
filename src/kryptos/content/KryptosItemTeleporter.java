d4d189a82465a91e9459097b1411aee208559f93
Ainss.Code <ainstheundeads@gmail.com>
2026-07-28 18:21:10 +0800

    Update KryptosItemTeleporter.java

--- src/kryptos/content/KryptosItemTeleporter.java
+++ src/kryptos/content/KryptosItemTeleporter.java
@@ -1,11 +1,16 @@
 package kryptos.content;
 
+import arc.graphics.g2d.Draw;
+import arc.util.io.Reads;
+import arc.util.io.Writes;
 import mindustry.content.Fx;
 import mindustry.gen.Building;
 import mindustry.type.Item;
 import mindustry.world.Block;
 import mindustry.world.blocks.storage.CoreBlock.CoreBuild;
 import mindustry.world.meta.BuildVisibility;
+
+import static mindustry.Vars.content;
 
 /**
  * A block that accepts any item and instantly moves it into the owning
@@ -33,6 +38,12 @@ public class KryptosItemTeleporter extends Block {
 
     public class KryptosItemTeleporterBuild extends Building {
 
+        // Whatever item most recently passed through -- drawn on the block
+        // in place of a fixed icon, so the block visually reflects whatever
+        // it's actually moving (copper if fed copper, titanium if fed
+        // titanium, etc.) instead of always showing the same static art.
+        public Item lastItem;
+
         @Override
         public boolean acceptItem(Building source, Item item) {
             // Accept anything from our own team, regardless of type or
@@ -46,11 +57,38 @@ public class KryptosItemTeleporter extends Block {
             CoreBuild core = team.core();
             if (core != null) {
                 core.items.add(item, 1);
+                lastItem = item;
                 Fx.teleportOut.at(x, y, 0f, item.color);
                 Fx.teleport.at(core.x, core.y, 0f, item.color);
             }
             // Intentionally not calling items.add(...) here -- the item is
             // teleported, not stored locally.
+        }
+
+        @Override
+        public void draw() {
+            super.draw();
+            if (lastItem != null) {
+                Draw.rect(lastItem.fullIcon, x, y, 8f, 8f);
+            }
+        }
+
+        @Override
+        public byte version() {
+            return 1;
+        }
+
+        @Override
+        public void write(Writes write) {
+            super.write(write);
+            write.s(lastItem == null ? -1 : lastItem.id);
+        }
+
+        @Override
+        public void read(Reads read, byte revision) {
+            super.read(read, revision);
+            short itemId = read.s();
+            lastItem = itemId == -1 ? null : content.item(itemId);
         }
     }
 }
